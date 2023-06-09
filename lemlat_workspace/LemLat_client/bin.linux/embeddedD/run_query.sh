3d178f7b389a24c94663ce58fd843d1408be153d
Andreas Büttner <andbue@gmail.com>
2023-06-09 14:44:51 +0200

    Speed up DB by running mysql optimization

--- lemlat_workspace/LemLat_client/bin.linux/embeddedD/run_query.sh
+++ lemlat_workspace/LemLat_client/bin.linux/embeddedD/run_query.sh
@@ -1,3 +1,4 @@
 bin/mysql --defaults-file=my_gen.cnf -h127.0.0.1 -uroot -P63301 -e "CREATE DATABASE IF NOT EXISTS lemlat_db"
 bin/mysql --defaults-file=my_gen.cnf -h127.0.0.1 -uroot -P63301 lemlat_db < $1
+bin/mysqlcheck --defaults-file=my_gen.cnf -h127.0.0.1 -uroot -P63301 -o --all-databases
 
